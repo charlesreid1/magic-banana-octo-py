fd6fd1628fbcd6050d9dcf2ea7d022d030978082
Charles Reid <charlesreid1@gmail.com>
2017-11-24 00:57:58 -0800

    Hopefully this will fix tox.

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -1,14 +1,18 @@
 import banana_octo_py as bop
 
+
 def test_core():
-    assert bop.hello_core()=="Hello world! This is the core.py file"
+    assert bop.hello_core() == "Hello world! This is the core.py file"
+
 
 def test_utils():
-    assert bop.utils.hello_utils()=="Hello world! This is the utils.py file"
+    assert bop.utils.hello_utils() == "Hello world! This is the utils.py file"
+
 
 def test_sub():
-    assert bop.sub.hello_submodule()=="Hello world! This is the submodule!"
-    assert bop.sub.one.hello_submodule_one()=="Hello world! This is the submodule file, one.py!"
-    assert bop.sub.two.hello_submodule_two()=="Hello world! This is the submodule file, two.py!"
-
-
+    assert bop.sub.hello_submodule() == \
+        "Hello world! This is the submodule!"
+    assert bop.sub.one.hello_submodule_one() == \
+        "Hello world! This is the submodule file, one.py!"
+    assert bop.sub.two.hello_submodule_two() == \
+        "Hello world! This is the submodule file, two.py!"
